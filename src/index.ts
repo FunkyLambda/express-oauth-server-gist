a7d77c3690e36d79ca0b0c50b583cb44d8cffe1d
Ash <44832227+FunkyLambda@users.noreply.github.com>
2021-07-04 19:23:09 +1000

    index.ts updated with new issue.

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,81 @@
 var expressApp = require('express')
 import express from 'express'
-import OAuthServer from 'express-oauth-server'
-import {AuthorizationCode, Client, User} from 'oauth2-server'
+import path from 'path'
+import ExpressOAuthServer from 'express-oauth-server'
+import {AuthorizationCode, Client, User, Token, RefreshToken, OAuthError} from 'oauth2-server'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import csrf from 'csurf'
 
 
 const app = expressApp()
-
-app.oauth = new OAuthServer({
-  model: {
-    getClient: async (clientId: string, clientSecret: string) => {
-      console.log('Reached getClient')
-      
-      if (clientId !== '1234')
-        return null
-      if (clientSecret && clientSecret !== 'abcd')
-        return null
-
-      return {
-        id: clientId,
-        redirectUris: ["http://localhost:4000", "http://localhost:5000"],
-        grants: ["authorization_code", "refresh_token"],
-        accessTokenLifetime: 3600 * 24, // 1 day
-        refreshTokenLifetime: 3600 * 24 * 30, // 30 days
-      }
-    },
-    saveAuthorizationCode: async (code: AuthorizationCode, client: Client, user: User) => {
-      console.log('Reached saveAuthorizationCode')
-      
-      return {
-        authorizationCode: code.authorizationCode,
-        expiresAt: code.expiresAt,
-        redirectUri: code.redirectUri,
-        scope: code.scope,
-        client: client,
-        user: user,
-      }
-    },
-    getAccessToken: async (accessTokenKey: string) => {
-      console.log('Reached getAccessToken')
-      
-      if (accessTokenKey.startsWith('XYZ'))
-        return null
-
-      const expiry_date = new Date()
-      expiry_date.setHours(expiry_date.getHours() + 1)
-
-      return {
-        accessToken: accessTokenKey,
-        accessTokenExpiresAt: expiry_date,
-        scope: ["email", "profile", "openid"],
-        client: {
-          id: '1234',
-          redirectUris: ["http://localhost:4000", "http://localhost:5000"],
-          grants: ["authorization_code", "refresh_token"],
-          accessTokenLifetime: 3600 * 24, // 1 day
-          efreshTokenLifetime: 3600 * 24 * 30, // 30 days
-        },
-        user: {
-          id: 234567,
-          email: 'foo@bar.com',
-        },
-      }
-    },
-  } as any, // Just to avoid TS errors.
-  //continueMiddleware: true,
-})
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(cookieParser())
+// TODO: Any key besides _csrf does not seem to work and generates an invalid csrf error on post.
+const csrfProtection = csrf({cookie: {key: '_csrf'}})
 
-app.use(app.oauth.authorize())  // Commenting this link obviously makes /public accessible.
-// TODO: Access this page without getting a 401, while also using the oauth middleware.
-app.get('/public', function(req: any, res: any) {
-  console.log('Reached /public')
-  res.send('Public area');
+class ValidationError extends Error {}
+class RegistrationVerificationError extends Error {}
+
+const getFooHandler = (req: express.Request, res: express.Response, _next: express.NextFunction) => {
+  console.log('Running GET:/foo ...')
+
+  const query_string = 'foo=abc&bar=xzy123'
+
+  res.render(
+    'foo',
+    {
+      queryString: query_string,
+      csrfToken: req.csrfToken(),
+    }
+  )
+}
+
+const postFooHandler = (req: express.Request, res: express.Response, _next: express.NextFunction) => {
+  console.log('Running POST:/foo ...')
+
+  res.render('Success')
+}
+
+const errorHandler = async (error: Error, _req: express.Request, res: express.Response, next: express.NextFunction) => {
+  if (error) {
+    console.log('Error', error)
+
+    let status_code = 500
+    let error_message = "An error occurred."
+    if (
+      error instanceof ValidationError ||
+      error instanceof RegistrationVerificationError
+    ) {
+      status_code = 400
+      error_message = error.message
+    } else if (error.name === 'ForbiddenError') {
+      // TODO: Can instanceof be used instead of comparing name?
+      status_code = 403
+      error_message = error.message
+    }
+
+    res.status(status_code)
+    .render(
+      'error',
+      {
+        message: error_message,
+      }
+    )
+  } else {
+    next()
+  }
+}
+
+app.listen(8080, () => {
+  console.log('Running server at port 8080...')
 })
 
-const port = 8080
-app.listen(port, () => {
-  console.log('Running server at port ' + port + '...')
-})
+// Endpoints
+app.get('/foo', csrfProtection, getFooHandler)
+app.post('/foo', csrfProtection, postFooHandler)
+
+app.use(errorHandler)
